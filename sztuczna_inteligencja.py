f0f6d283ca834a6480943d6145b213a79d3f57a7
GoldenShadow42 <oskar.szalach@gmail.com>
2023-11-23 16:48:44 +0100

    aktualizacja

--- sztuczna_inteligencja.py
+++ sztuczna_inteligencja.py
@@ -4,7 +4,52 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-def train_and_evaluate_model(data_path='Heart Attack.csv', target_column='class', test_size=0.2, random_state=0):
+class HeartAttackApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Heart Attack Prediction App")
+
+        # Tworzenie etykiet i pól do wprowadzania danych
+        self.labels = ['Age:', 'Gender:', 'Impulse:', 'Pressure High:', 'Pressure Low:', 'Glucose:', 'KCM:', 'Troponin:']
+        self.entries = []
+
+        for i, label in enumerate(self.labels):
+            tk.Label(master, text=label).grid(row=i, column=0, pady=5)
+            entry = tk.Entry(master)
+            entry.grid(row=i, column=1, pady=5)
+            self.entries.append(entry)
+
+        # Przycisk do uruchamiania modelu
+        tk.Button(master, text="Sprawdz", command=self.predict).grid(row=len(self.labels) + 1, column=1, pady=10)
+
+        # Pole tekstowe do wyświetlania wyniku
+        self.result_text = tk.Text(master, height=2, width=30)
+        self.result_text.grid(row=len(self.labels) + 2, column=0, columnspan=2, pady=10)
+        self.result_text.config(state=tk.DISABLED)
+
+        # Ustawienie najlepszego modelu poza klasą
+        self.best_model = self.train_best_model()
+
+    def train_best_model(self):
+        licznik = 30
+        best_accuracy = 0
+        best_model = None
+
+        while licznik <= 1000:
+            print(licznik)
+            current_accuracy, current_model = self.train_and_evaluate_model(random_state=licznik)
+
+            if current_accuracy > best_accuracy:
+                best_accuracy = current_accuracy
+                best_model = current_model
+            if best_accuracy == 1:
+                break
+
+            licznik += 1
+
+        return best_model
+
+    def train_and_evaluate_model(self, data_path='Heart Attack.csv', target_column='class', test_size=0.2, random_state=0):
     # Wczytujemy dane z pliku CSV
     df = pd.read_csv(data_path)
 
